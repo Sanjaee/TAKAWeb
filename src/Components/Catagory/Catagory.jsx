c7f8b37725002ef8efac0194eb657adec0d08bef
Sanjaee <afrizaahmad18@gmail.com>
2024-01-14 16:06:06 +0700

    revissi3

--- src/Components/Catagory/Catagory.jsx
+++ src/Components/Catagory/Catagory.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "../../Utils/Firebase";
 import "./Catagory.css";
 
 export const Catagory = () => {
   const [catagoryData, setCatagoryData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     const catagoryCollection = collection(db, "Catagory");
+    const q = query(
+      catagoryCollection,
+      selectedCategory ? where("catagory", "==", selectedCategory) : null
+    );
 
-    const unsubscribe = onSnapshot(catagoryCollection, (querySnapshot) => {
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const catagoryItems = [];
       querySnapshot.forEach((doc) => {
         catagoryItems.push({ id: doc.id, ...doc.data() });
@@ -21,13 +26,46 @@ export const Catagory = () => {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [selectedCategory]);
+
+  const handleButtonClick = (category) => {
+    setSelectedCategory(category);
+  };
 
   return (
     <div className="catagory">
       <div className="list">
         <div className="btn_catagory">
-          <button>Top Up Games</button>
+          <button
+            className={selectedCategory === "topup" ? "active" : ""}
+            onClick={() => handleButtonClick("topup")}
+          >
+            Top Up Games
+          </button>
+          <button
+            className={selectedCategory === "mobilelegend" ? "active" : ""}
+            onClick={() => handleButtonClick("mobilelegend")}
+          >
+            Mobile Legend
+          </button>
+          <button
+            className={selectedCategory === "voucer" ? "active" : ""}
+            onClick={() => handleButtonClick("voucer")}
+          >
+            Voucher
+          </button>
+          <button
+            className={selectedCategory === "tagihan" ? "active" : ""}
+            onClick={() => handleButtonClick("tagihan")}
+          >
+            Pulsa, Data & Tagihan
+          </button>
+          <button
+            className={selectedCategory === "entertaiment" ? "active" : ""}
+            onClick={() => handleButtonClick("entertaiment")}
+          >
+            Entertainment
+          </button>
         </div>
         <div className="list_catagory">
           {catagoryData.map((catagoryItem) => (
